d5b46b594e2c6bd1010422d8be15e77e5b14d1e5
Биточкин Егор <e.bitochkin@g.nsu.ru>
2024-11-25 13:36:50 +0700

    commit

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 		valuesArray := make([]table.ParameterOption, len(values)+1)
 		valuesArray[0] = table.ValueParam("$"+tableIndex, types.Uint64Value(i))
 		for j := 0; j < len(values); j++ {
+			fmt.Println("$"+field[j], values[j])
 			valuesArray[j+1] = table.ValueParam("$"+field[j], types.TextValue(values[j]))
 		}
 		err = db.ExecuteWithParams(query, table.NewQueryParameters(valuesArray...))
